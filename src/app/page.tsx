960b84086e224874caa26dcebd0eedbf93a5c594
varekai-dev <serhijsav@gmail.com>
2025-11-12 10:01:54 +0200

    Update dependencies and improve button component styling
    - Added Prisma and TSX as dependencies in package.json and package-lock.json.
    - Updated .gitignore to exclude generated Prisma files.
    - Enhanced button component with improved styling and structure in button.tsx.
    - Modified page.tsx to utilize the new button component for better UI.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 const Page = () => {
-  return <div className="text-red-500">hello</div>;
+  return (
+    <div className="min-h-screen min-w-screen flex items-center justify-center">
+      <Button variant="destructive">Click me</Button>
+    </div>
+  );
 };
 
 export default Page;
