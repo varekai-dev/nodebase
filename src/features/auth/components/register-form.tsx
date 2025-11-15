18f10eba8a501d3ae4b15b55dafe61ed9127b452
varekai-dev <serhijsav@gmail.com>
2025-11-15 23:50:13 +0200

    5: theme & styling

--- src/features/auth/components/register-form.tsx
+++ src/features/auth/components/register-form.tsx
@@ -95,6 +95,12 @@ export const RegisterForm = () => {
                     type="button"
                     disabled={isPending}
                   >
+                    <Image
+                      src="./logos/github.svg"
+                      alt="GitHub"
+                      width={20}
+                      height={20}
+                    />
                     Continue with GitHub
                   </Button>
                   <Button
@@ -103,6 +109,12 @@ export const RegisterForm = () => {
                     type="button"
                     disabled={isPending}
                   >
+                    <Image
+                      src="./logos/google.svg"
+                      alt="Google"
+                      width={20}
+                      height={20}
+                    />
                     Continue with Google
                   </Button>
                 </div>
