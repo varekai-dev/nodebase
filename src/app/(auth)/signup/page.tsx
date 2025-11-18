fd888bd6839c042f4bba67fe493c29e0bd7f462c
Serhii Saviuk <60782198+varekai-dev@users.noreply.github.com>
2025-11-18 10:37:03 +0200

    Revert "one tap auth"

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,15 +1,9 @@
-import { OneTap } from "@/features/auth/components/one-tap";
 import { RegisterForm } from "@/features/auth/components/register-form";
 import { requireUnauth } from "@/lib/auth-utils";
 
 const Page = async () => {
   await requireUnauth();
-  return (
-    <>
-      <OneTap />
-      <RegisterForm />
-    </>
-  );
+  return <RegisterForm />;
 };
 
 export default Page;
--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -6,7 +6,19 @@ export const authClient = createAuthClient({
   plugins: [
     polarClient(),
     oneTapClient({
-      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID as string,
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      // Optional client configuration:
+      autoSelect: false,
+      cancelOnTapOutside: true,
+      context: "signin",
+      additionalOptions: {
+        // Any extra options for the Google initialize method
+      },
+      // Configure prompt behavior and exponential backoff:
+      promptOptions: {
+        baseDelay: 1000, // Base delay in ms (default: 1000)
+        maxAttempts: 5, // Maximum number of attempts before triggering onPromptNotification (default: 5)
+      },
     }),
   ],
 });
