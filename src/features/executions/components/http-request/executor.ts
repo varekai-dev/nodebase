b747ed89409cc24cf8028eac6d0536568976db0e
varekai-dev <serhijsav@gmail.com>
2025-11-30 18:10:12 +0200

    20: node templating

--- src/features/executions/components/http-request/executor.ts
+++ src/features/executions/components/http-request/executor.ts
@@ -1,12 +1,18 @@
+import Handlebars from "handlebars";
 import { NonRetriableError } from "inngest";
 import ky, { type Options as KyOptions } from "ky";
 import type { NodeExecutor } from "@/features/executions/types";
 
+Handlebars.registerHelper("json", (context) => {
+  const jsonString = JSON.stringify(context, null, 2);
+  return new Handlebars.SafeString(jsonString);
+});
+
 type HttpRequestData = {
-  endpoint?: string;
-  method?: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
+  endpoint: string;
+  method: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
   body?: string;
-  variableName?: string;
+  variableName: string;
 };
 
 export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
@@ -18,32 +24,33 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
   // TODO Publish 'loading' state for http request
 
   if (!data.endpoint) {
-    // TODO Publish 'error' state for http request
-    throw new NonRetriableError("Endpoint is required");
+    throw new NonRetriableError("HTTP Request: No endpoint configured");
+  }
+
+  if (!data.variableName) {
+    throw new NonRetriableError("HTTP Request: No variable name configured");
+  }
+
+  if (!data.method) {
+    throw new NonRetriableError("HTTP Request: No method configured");
   }
 
   const result = await step.run("http-request", async () => {
-    if (!data.endpoint) {
-      throw new NonRetriableError("HTTP Request: No endpoint configured");
-    }
-
-    if (!data.variableName) {
-      throw new NonRetriableError("HTTP Request: No variable name configured");
-    }
-    const method = data.method || "GET";
-    const endpoint = data.endpoint;
+    const method = data.method;
+    const endpoint = Handlebars.compile(data.endpoint)(context);
 
     const options: KyOptions = {
       method,
     };
 
     if (["POST", "PUT", "PATCH"].includes(method)) {
-      if (data.body) {
-        options.body = data.body;
-        options.headers = {
-          "Content-Type": "application/json",
-        };
-      }
+      const resolved = Handlebars.compile(data.body || "{}")(context);
+      JSON.parse(resolved);
+
+      options.body = resolved;
+      options.headers = {
+        "Content-Type": "application/json",
+      };
     }
 
     const response = await ky(endpoint, options);
